01f9eb75205ea3b5acd67146f630a48cbb48f912
Ken Eucker <keneucker@gmail.com>
2020-11-12 11:21:49 -0800

    Updated http-error-pages to use more features. Error pages no longer show up when running in debug mode and errors are logged to the console in production mode.

--- examples/advanced/controllers/api/index.js
+++ examples/advanced/controllers/api/index.js
@@ -1,7 +1,7 @@
 const routes = (app) => {
     app.routeSubdomainRequest('/v3/:endpoint?', (subdomain, req, res, host, next) => {
         const myError = new Error()
-		myError.status = 501
+        myError.status = 501
         myError.message = 'Version 3 not yet implemented'
         next(myError)
     })
