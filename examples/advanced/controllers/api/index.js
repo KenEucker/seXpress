fa710c2cfa4273bde85457b7df9c580d205f7e93
Ken Eucker <keneucker@gmail.com>
2020-11-12 22:39:33 -0800

    Added autogenerated swagger docs at the endpoint /api/docs as well as a client dropped into /public/js/appName-api.js.

--- examples/advanced/controllers/api/index.js
+++ examples/advanced/controllers/api/index.js
@@ -4,7 +4,33 @@ class ApiController {
         this.engine = 'ejs'
     }
 
+    /**
+     * @swagger
+     * /v1/yo/{yo}:
+     *   post:
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - in: path
+     *         name: yo
+     *         description: the tag nunber to retrieve
+     *         schema:
+     *           type: integer
+     *     description: Retrieves the reddit post template for the given tag number, or latest
+     *     responses:
+     *       200:
+     *         description: reddit post text
+     */
+    yo(subdomain, req, res, host, next) {
+		return res.json({
+			yo: req.params.yo,
+			subdomain,
+			host,
+		})
+	}
+
     routes(app) {
+        app.routeSubdomainRequest('/v1/yo/:yo?', this.yo, 'post')
         app.routeSubdomainRequest('/v3/:endpoint?', function NotImplemented(
             subdomain,
             req,
@@ -17,7 +43,7 @@ class ApiController {
             myError.message = 'Version 3 not yet implemented'
 
             next(myError)
-        })
+        }, 'post')
     }
 }
 
