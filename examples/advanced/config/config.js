d504d64e4f53a1eb4f12d623635f2f95e98ed304
Ken Eucker <keneucker@gmail.com>
2020-11-26 14:00:35 -0800

    Added redis for session storage, though it's not working.

--- examples/advanced/config/config.js
+++ examples/advanced/config/config.js
@@ -7,7 +7,13 @@ module.exports = {
     },
 
     login: true,
-    authentication: true,
+	authentication: true,
+	
+	session: {
+		redis: {
+			// enabled: true,
+		},
+	},
 
     logging: {
         onlyLogErrors: false,
